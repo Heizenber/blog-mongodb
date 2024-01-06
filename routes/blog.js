244b9181ddaf64dcb863f788fa81cbb253946260
Heizenberg <popov.roman91@gmail.com>
2024-01-06 10:48:44 +0200

    all the posts are now listed on posts endpoint

--- routes/blog.js
+++ routes/blog.js
@@ -1,17 +1,43 @@
 const express = require('express');
+const db = require('../data/database');
+const mongodb = require('mongodb');
+
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get('/posts', async (req, res) => {
+  const posts = await db.getDb().collection('posts').find({}, {title: 1, summary: 1, 'author.name': 1}).toArray()
+  res.render('posts-list', {posts: posts});
 });
 
-router.get('/new-post', async function(req, res) {
-  res.render('create-post');
+router.get('/new-post', async (req, res) => {
+  let authors = await db.getDb().collection('authors').find().toArray()
+  res.render('create-post', {authors: authors});
+});
+
+router.post('/posts', async (req, res) => {
+  const authorId = new ObjectId(req.body.author)
+  const authorData = await db.getDb().collection('authors').findOne({_id: authorId})
+
+  const post = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: authorData.name,
+      email: authorData.email},
+  }
+  const result = await db.getDb().collection('posts').insertOne(post)
+  console.log(result)
+  res.redirect('/posts')
+
 });
 
 module.exports = router;
